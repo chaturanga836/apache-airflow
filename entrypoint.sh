b9dafbb47cd0e0e68bca7c4840227cf873548989
chathuranga836@gmail.com <chaturanga836@gmail.com>
2026-01-23 20:10:59 +0530

    f

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ gosu airflow airflow db migrate
 # 3. AUTOMATED ADMIN CREATION
 echo "Ensuring Admin user exists..."
 #Use the new provider-based command structure
-gosu airflow airflow fab sync-perm
+gosu airflow airflow sync-perm
 
 gosu airflow airflow providers fab users-create \
     --username admin \
