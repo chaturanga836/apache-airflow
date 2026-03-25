5eacf957441d54ef9e6e9c255f819a842e85b5d8
chathuranga836@gmail.com <chaturanga836@gmail.com>
2026-03-26 00:31:24 +0530

    dag

---
+++ dags/hudi_elt_pipeline.py
@@ -0,0 +1,12 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+with DAG(
+    dag_id='hudi_elt_pipeline',  # <--- This must match Postman exactly
+    start_date=datetime(2025, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=['hudi', 'elt']
+) as dag:
+    trigger_test = EmptyOperator(task_id='trigger_test')
